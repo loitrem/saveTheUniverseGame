a7a977b5e577850f5cecbf010b1b4af7ac9623fa
loitrem <ski_fix@hotmail.com>
2023-06-20 10:23:54 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,87 @@
+// save the universe
+
+// set hero ship stats
+let heroHull = 20;
+let heroFirepower = 5;
+let heroAccuracy = 0.7;
+
+// set enemy ship stats
+const enemyStats = () => {
+    // random for hull
+
+    // random for firepower
+
+    // ramdon for accuracy
+
+}
+
+// hero ship class
+class heroShip {
+
+    // constructor
+    constructor () {
+
+    }
+
+    // attack
+    attack() {
+
+    }
+
+    // getDamaged
+    getDamaged() {
+
+    }
+
+    // retreat
+    retreat() {
+
+    }
+
+}
+
+
+// enemy ship class
+class enemyShip {
+
+    // constructor
+    constructor () {
+        
+    }
+
+    // attack
+    attack() {
+
+    }
+
+    // getDamaged
+    getDamaged() {
+        
+    }
+
+}
+
+// set enemy ship array
+
+
+// enemy array
+let enemyFleet = [];
+
+// combat
+const combat = (ship) => {
+
+    // attack
+    const attack = () => {
+
+    }
+
+    // getDamaged
+    const damaged = () => {
+
+    }
+
+}
+
+
+
+
